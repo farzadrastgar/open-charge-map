264207ec4976914e0596d11aed78226df55d3e35
Farzad Rastegarsani <farzadrastgar@gmail.com>
2024-07-26 14:31:27 +0200

    implement dotenv

--- src/config/kafkaConfig.ts
+++ src/config/kafkaConfig.ts
@@ -7,7 +7,7 @@ export type KafkaConfiguration = {
 
 export const kafkaConfiguration: KafkaConfiguration = {
   clientId: "kafka-listener",
-  brokers: ["localhost:9092"],
+  brokers: [process.env.KAFKA_BROKER || "localhost:9092"],
   consumerGroupId: "my-group",
   topic: "fetch_topic",
 };
