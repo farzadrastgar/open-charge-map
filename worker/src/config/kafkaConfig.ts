79b38339b2ad4b05cd704564d8a0665badcd6f00
Farzad Rastegarsani <farzadrastgar@gmail.com>
2024-07-28 16:09:33 +0200

    dockerize worker

--- worker/src/config/kafkaConfig.ts
+++ worker/src/config/kafkaConfig.ts
@@ -3,7 +3,7 @@ import { KafkaConfiguration } from "../types/kafka";
 
 export const kafkaConfiguration: KafkaConfiguration = {
   clientId: "kafka-listener",
-  brokers: [process.env.KAFKA_BROKER || "localhost:9092"],
+  brokers: [process.env.KAFKA_BROKER || "kafka1:9092"],
   consumerGroupId: "my-group",
   topic: "fetch_topic",
 };
